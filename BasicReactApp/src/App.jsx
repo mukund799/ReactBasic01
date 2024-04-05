f4b5606367cdeda295af95e279957bd4d84be3ac
MUKUND NARAYAN <mukundnarayan87@gmail.com>
2024-04-05 17:20:06 +0530

    first commit, set-up for react and made ui changes

---
+++ BasicReactApp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3> React with Mukund</h3>
+    </>
+  )
+}
+
+export default App
