a94b056ab75dcc3a329260a751f265dcfb5d4ca1
MUKUND NARAYAN <mukundnarayan87@gmail.com>
2024-04-05 17:52:54 +0530

    tried to use hooks usestate. with up and down buttton we can max increase the value to 20 and we can't ess than 0.

--- BasicReactApp/src/App.jsx
+++ BasicReactApp/src/App.jsx
@@ -3,9 +3,22 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
+  const up = () =>{
+    if(count >= 20){ alert(" count can't be greater than 20"); return}
+    setCount(count => count+1)
+    
+  }
+  const down = () =>{
+    if(count <=0){ alert(" count can't be lesser than 0"); return}
+    setCount(count -1)
+  }
   return (
     <>
       <h3> React with Mukund</h3>
+      <h1>Count is {count}</h1>
+      <button onClick={up}>Up</button>
+      <br></br>
+      <button onClick={down}>Down</button>
     </>
   )
 }
